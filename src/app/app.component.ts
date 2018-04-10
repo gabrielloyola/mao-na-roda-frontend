fd715be5c556d530b5941ac8e23eb6d66849d5e1
gabrielloyola <gabrielfrml@gmail.com>
2018-04-10 02:03:17 -0300

    Clusteringgg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mão na roda';
-  lat: number = -25.439253;
-  lng: number = -49.268903;
   locationChosen: boolean = false;
 
-  onChoseLocation(event) {
-    this.lat = event.coords.lat;
-    this.lng = event.coords.lng;
-    this.locationChosen = true;
-  }
+  lat: number =  -25.436011;
+  lng: number = -49.271747;
+
+  markers: any[] = [
+    {
+      "latitude": -25.436011,
+      "longitude": -49.271747
+    },
+    {
+      "latitude": -25.436204,
+      "longitude": -49.2720907
+    },
+    {
+      "latitude": -25.435559,
+      "longitude": -49.271601
+    },
+    {
+      "latitude": -25.470442,
+      "longitude": -49.245428
+    },
+    {
+      "latitude": -25.436185,
+      "longitude": -49.277133
+    },
+    {
+      "latitude": -25.434867,
+      "longitude": -49.281553
+    },
+    {
+      "latitude": -25.455158,
+      "longitude": -49.282377
+    },
+
+    {
+      "latitude": -25.455119,
+      "longitude": -49.288128
+    },
+    {
+      "latitude": -25.456016,
+      "longitude": -49.281024
+    },
+    {
+      "latitude": -25.458613,
+      "longitude": -49.284071
+    },
+  ]
 }
